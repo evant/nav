d9debaf4d0b3b97cebbd1dfc8b1fdb1c22710e50
Evan Tatarka <evan@tatarka.me>
2021-01-14 22:51:47 -0500

    Fix tests

--- nav/src/test/java/me/tatarka/nav/BackStackTest.kt
+++ nav/src/test/java/me/tatarka/nav/BackStackTest.kt
@@ -1,5 +1,7 @@
 package me.tatarka.nav
 
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import assertk.Assert
 import assertk.assertThat
 import assertk.assertions.containsExactly
 import org.junit.Test
@@ -8,34 +10,42 @@ class BackStackTest {
 
     @Test
     fun `popTo non inclusive pops to the given page before adding`() {
-        val backStack = BackStack(mutableListOf("one", "two"))
+        val backStack = BackStack(snapshotListOf("one", "two"))
         backStack.navigate("three", popUpTo = { it == "one" })
 
-        assertThat(backStack.pages).containsExactly("one", "three")
+        assertThat(backStack.pages).containsExactly2("one", "three")
     }
 
     @Test
     fun `popTo inclusive pops the given page before adding`() {
-        val backStack = BackStack(mutableListOf("one", "two"))
+        val backStack = BackStack(snapshotListOf("one", "two"))
         backStack.navigate("three", popUpTo = { it == "two" }, inclusive = true)
 
-        assertThat(backStack.pages).containsExactly("one", "three")
+        assertThat(backStack.pages).containsExactly2("one", "three")
     }
 
     @Test
     fun `singleTop adds page if it is not already there`() {
-        val backStack = BackStack(mutableListOf("one"))
+        val backStack = BackStack(snapshotListOf("one"))
         backStack.navigate("two", singleTop = true)
 
-        assertThat(backStack.pages).containsExactly("one", "two")
+        assertThat(backStack.pages).containsExactly2("one", "two")
     }
 
     @Test
     fun `singleTop skips adding page if it is already there`() {
-        val backStack = BackStack(mutableListOf("one"))
+        val backStack = BackStack(snapshotListOf("one"))
         backStack.navigate("one", singleTop = true)
 
-        assertThat(backStack.pages).containsExactly("one")
+        assertThat(backStack.pages).containsExactly2("one")
     }
 
+    private fun <T> snapshotListOf(vararg elements: T) = SnapshotStateList<T>().apply {
+        addAll(elements)
+    }
+
+    // https://github.com/willowtreeapps/assertk/issues/332
+    private fun Assert<List<*>>.containsExactly2(vararg elements: Any?) {
+        transform { it.toList() }.containsExactly(*elements)
+    }
 }
