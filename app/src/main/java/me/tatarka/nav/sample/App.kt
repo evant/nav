a28a45e24b8d59e4cf8d0113c5e912cf814a1581
Evan Tatarka <evan@tatarka.me>
2021-05-22 16:11:15 -0400

    Add transition animation support (#1)
    * Add transition animation support
    
    * Fix tests

--- app/src/main/java/me/tatarka/nav/sample/App.kt
+++ app/src/main/java/me/tatarka/nav/sample/App.kt
@@ -2,6 +2,8 @@ package me.tatarka.nav.sample
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.*
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
@@ -11,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -75,9 +78,9 @@ fun App(backStack: AppBackStack) {
                 }
             }
         }) { padding ->
-//        Navigator(backStack.primary) {
+        Navigator(backStack.primary) { page ->
             Box(Modifier.padding(padding)) {
-                when (backStack.primary.current) {
+                when (page) {
                     Page.Search -> {
                         Search(backStack.search)
                     }
@@ -89,68 +92,107 @@ fun App(backStack: AppBackStack) {
                     }
                 }
             }
-//        }
+        }
     }
 }
 
 @Composable
+@OptIn(ExperimentalAnimationApi::class)
 fun Search(backStack: BackStack<SearchPage>) {
-    Navigator(backStack) {
-        when (val page = backStack.current) {
-            SearchPage.Main -> {
-                var query by rememberSaveable {
-                    println("new query state")
-                    mutableStateOf("")
-                }
-                TextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = query,
-                    onValueChange = { query = it },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                    keyboardActions = KeyboardActions {
+    Navigator(backStack) { page ->
+        pageTransition.AnimatedVisibility(
+            visible = { page == it },
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            when (page) {
+                SearchPage.Main -> {
+                    SearchMain(onSearch = { query ->
                         backStack.navigate(SearchPage.Results(query), singleTop = true)
-                    }
-                )
-            }
-            is SearchPage.Results -> {
-                Box(Modifier.fillMaxSize()) {
-                    Text(
-                        modifier = Modifier.align(Alignment.Center),
-                        textAlign = TextAlign.Center,
-                        text = page.query
-                    )
+                    })
+                }
+                is SearchPage.Results -> {
+                    SearchResults(text = page.query)
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SearchMain(onSearch: (query: String) -> Unit) {
+    var query by rememberSaveable(key = "query") { mutableStateOf("") }
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = query,
+        onValueChange = { query = it },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions { onSearch(query) }
+    )
+}
+
+@Composable
+fun SearchResults(text: String) {
+    Box(Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            textAlign = TextAlign.Center,
+            text = text
+        )
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Home(backStack: BackStack<HomePage>) {
-    Navigator(backStack) {
-        when (val page = backStack.current) {
+    Navigator(backStack) { page ->
+        when (page) {
             HomePage.List -> {
-                Column {
-                    for (i in 0 until 10) {
-                        ListItem(Modifier.clickable(onClick = {
-                            backStack.navigate(HomePage.Detail(i))
-                        })) {
-                            Text("Item: $i")
-                        }
-                    }
+                pageTransition.AnimatedVisibility(
+                    visible = { it == page },
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    HomeList(onItemClick = { item ->
+                        backStack.navigate(HomePage.Detail(item))
+                    })
                 }
             }
             is HomePage.Detail -> {
-                Box(Modifier.fillMaxSize()) {
-                    Text(
-                        modifier = Modifier.align(Alignment.Center),
-                        textAlign = TextAlign.Center,
-                        text = "Detail: ${page.id}"
-                    )
+                pageTransition.AnimatedVisibility(
+                    visible = { it == page },
+                    enter = slideInHorizontally(),
+                    exit = slideOutHorizontally() + fadeOut(),
+                ) {
+                    HomeDetail(id = page.id)
                 }
             }
         }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+fun HomeList(onItemClick: (item: Int) -> Unit) {
+    Column {
+        for (i in 0 until 10) {
+            ListItem(Modifier.clickable(onClick = {
+                onItemClick(i)
+            })) {
+                Text("Item: $i")
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeDetail(id: Int) {
+    Box(Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            textAlign = TextAlign.Center,
+            text = "Detail: $id"
+        )
     }
 }
 
